df9b2c50f4b34fa63f739957bc7c7414a514b98c
Andres Giraldo Arenas <andresganc@gmail.com>
2022-11-26 11:27:55 -0500

    New: Card Test Tailwinds

--- tailwindcss/exercises/spa-react-tailwind-css/src/App.jsx
+++ tailwindcss/exercises/spa-react-tailwind-css/src/App.jsx
@@ -1,20 +1,23 @@
 
 import { useState } from 'react'
 
+// Images
+import ImageProfile from '/img/capitan-america.webp'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main className='inline-block bg-slate-600'>
-      <div>
-        <img src="" alt="" />
+    <main className='inline-block p-5 bg-violet-500 rounded-2xl' >
+      <div className='container margin-auto'>
+        <img src={ImageProfile} alt="" className='w-32' />
       </div>
       <div>
         <h4 className='text-center text-white font-bold'>SPA REACT TAILWIND CSS</h4>
         <p className='text-center'>Lorem, ipsum dolor.</p>
       </div>
       <div className=''>
-        <button className='bg-blue-400'>Ver</button>
+        <button className='px-3 rounded-sm bg-blue-400 '>Ver</button>
       </div>
     </main>
   )
